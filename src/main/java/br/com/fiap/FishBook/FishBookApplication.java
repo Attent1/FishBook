693fdd57355549706d308920a29910b42e94a628
Attent1 <rafaeldsc22@outlook.com>
2024-05-27 21:03:59 -0300

    criando projeto

---
+++ src/main/java/br/com/fiap/FishBook/FishBookApplication.java
@@ -0,0 +1,22 @@
+package br.com.fiap.FishBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class FishBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishBookApplication.class, args);
+	}
+ 
+	@RequestMapping		
+	@ResponseBody
+	public String home(){
+		return "FishBook";
+	}
+}
